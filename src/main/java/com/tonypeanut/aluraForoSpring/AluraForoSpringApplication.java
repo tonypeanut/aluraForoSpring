a3d41d2f548dbc7662eafc88891b59f796d25faf
tonypeanut <tonypeanut93@gmail.com>
2024-06-19 16:09:42 -0700

    Creación del repositorio

---
+++ src/main/java/com/tonypeanut/aluraForoSpring/AluraForoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tonypeanut.aluraForoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraForoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraForoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonypeanut/aluraForoSpring/AluraForoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonypeanut.aluraForoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraForoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
